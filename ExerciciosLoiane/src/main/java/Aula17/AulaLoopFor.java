b81b0ac65445e17f57f7fea9cc8f787f515713dd
mfernandasdias <91575360+mfernandadias@users.noreply.github.com>
2024-05-01 21:59:55 -0300

    loop for

---
+++ ExerciciosLoiane/src/main/java/Aula17/AulaLoopFor.java
@@ -0,0 +1,20 @@
+package Aula17;
+import java.util.Random;
+import java.util.Scanner;
+
+public class AulaLoopFor {
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+        Random generate = new Random();
+
+        //loop infinito
+         int i = 0;
+        while(i < 6){
+            int number = generate.nextInt(60);
+            System.out.println(number);
+            i++;
+
+        }
+
+    }
+}
