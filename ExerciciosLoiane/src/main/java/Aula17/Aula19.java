348b27d92a07e2aebf553ba399350556417d73ff
mfernandasdias <91575360+mfernandadias@users.noreply.github.com>
2024-05-05 17:32:57 -0300

    Exercício 18

---
+++ ExerciciosLoiane/src/main/java/Aula17/Aula19.java
@@ -0,0 +1,11 @@
+package Aula17;
+import java.util.Scanner;
+/*
+faça um programa que calcule o fatorial de um número inteiro fornecido pelo usuário
+ */
+
+public class Aula19 {
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+    }
+}
---
+++ ExerciciosLoiane/src/main/java/Aula17/Exerc18.java
@@ -0,0 +1,10 @@
+package Aula17;
+import java.util.Scanner;
+
+public class Exerc18 {
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+
+        
+    }
+}
