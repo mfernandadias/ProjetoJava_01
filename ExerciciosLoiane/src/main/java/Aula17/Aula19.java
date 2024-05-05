552073910ccbc1be15c5de84fe659887e54b4dab
mfernandasdias <91575360+mfernandadias@users.noreply.github.com>
2024-05-05 19:00:41 -0300

    fatorial

--- ExerciciosLoiane/src/main/java/Aula17/Aula19.java
+++ ExerciciosLoiane/src/main/java/Aula17/Aula19.java
@@ -7,5 +7,17 @@ faça um programa que calcule o fatorial de um número inteiro fornecido pelo us
 public class Aula19 {
     public static void main(String[] args){
         Scanner scan = new Scanner(System.in);
+
+        System.out.println("Digite um número inteiro ");
+        int num = scan.nextInt();
+
+        int multiplicacao = 1;
+
+        System.out.println(num +"! =");
+        for (int i = num; i >= 1; i--){
+            multiplicacao = multiplicacao * i;
+        }
+        System.out.println(multiplicacao);
+
     }
 }
