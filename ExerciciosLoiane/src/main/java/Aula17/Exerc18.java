8737f8892af00575d6788245c20f618433ccc78d
mfernandasdias <91575360+mfernandadias@users.noreply.github.com>
2024-05-05 20:04:59 -0300

    Exerc 18 - pronto

--- ExerciciosLoiane/src/main/java/Aula17/Exerc18.java
+++ ExerciciosLoiane/src/main/java/Aula17/Exerc18.java
@@ -1,5 +1,9 @@
 package Aula17;
 import java.util.Scanner;
+/*
+Faca um programa que, dado um conjunto de N numeros, determine o
+menor valor, 0 maior valor e a soma dos valores.
+ */
 
 public class Exerc18 {
     public static void main(String[] args){
