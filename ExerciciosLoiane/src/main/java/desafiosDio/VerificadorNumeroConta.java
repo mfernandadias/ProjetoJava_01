eb40974b3be7463b46b268d2319d9562fc5e6355
mfernandasdias <91575360+mfernandadias@users.noreply.github.com>
2024-05-15 23:29:54 -0300

    VerificadorNumeroConta - inicio desafio

---
+++ ExerciciosLoiane/src/main/java/desafiosDio/VerificadorNumeroConta.java
@@ -0,0 +1,8 @@
+package desafiosDio;
+import java.util.Scanner;
+
+public class VerificadorNumeroConta {
+    public static void main(String[]args){
+        Scanner scan = new Scanner(System.in);
+    }
+}
