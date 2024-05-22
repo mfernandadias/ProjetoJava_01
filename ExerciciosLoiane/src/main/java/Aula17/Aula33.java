4d74c2d9273efa8ea17af5b7fecca9a17e283f52
mfernandasdias <91575360+mfernandadias@users.noreply.github.com>
2024-05-21 21:44:42 -0300

    exercício 33

--- ExerciciosLoiane/src/main/java/Aula17/Aula33.java
+++ ExerciciosLoiane/src/main/java/Aula17/Aula33.java
@@ -12,6 +12,27 @@ public class Aula33 {
     public static void main(String[]args){
         Scanner scan = new Scanner(System.in);
 
+        System.out.println("Digite um valor inteiro");
+        int num = scan.nextInt();
+
+        if (num <=1){
+            System.out.println(num + "Não é um número primo");
+        }else {
+            boolean ehPrimo = true;
+
+            for (int i =2; i <= num / 2; i++){
+                if (num % 1 == 0){
+                    ehPrimo = false;
+                    break;
+                }
+            }
+            if (ehPrimo){
+                System.out.println(num + " é um numero primo");
+            }else {
+                System.out.println(num + "não é um número primo");
+            }
+        }
+
 
 
     }
