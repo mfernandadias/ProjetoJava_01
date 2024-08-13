51aa83dad273db06829b58197fd18d9a5cdd94d3
mfernandasdias <91575360+mfernandadias@users.noreply.github.com>
2024-08-13 16:27:08 -0300

    Exercicio 55
    java - laço de repetição

--- ExerciciosLoiane/src/main/java/livro100Exercicios/LacosDeRepeticao/Exer55.java
+++ ExerciciosLoiane/src/main/java/livro100Exercicios/LacosDeRepeticao/Exer55.java
@@ -1,7 +1,7 @@
 package livro100Exercicios.LacosDeRepeticao;
 import java.util.Scanner;
 public class Exer55 {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         /*
         escreva um programa que solicite ao usuário um número N e diga se o mesmo é primo ou não
@@ -9,7 +9,33 @@ public class Exer55 {
         System.out.println("Digite um número inteiro: ");
         int num = scan.nextInt();
 
-        
-    }
+        System.out.println("Números primos menores que " + num  + ": ");
 
+        for (int i = 2; i < num; i++){
+            boolean isPrimo = true;
+            for(int j = 2; j < i; j++){
+                if(j % 2 == 0){
+                    isPrimo = false;
+                    break;
+                }
+            }
+            if(isPrimo){
+                System.out.println(i + " ");
+            }
+        }
+
+        /*for (int i = 2; i < num; i++) {
+            boolean isPrimo = true;
+
+            for (int j = 2; j < i; j++) {
+                if( i % j == 0){
+                isPrimo = false;
+                    break;
+                }
+            }
+            if (isPrimo) {
+                System.out.println(i + " ");
+            }
+        }*/
+    }
 }
