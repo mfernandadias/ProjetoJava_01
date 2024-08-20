7075829f70db509695efe1f377b08d5414d65918
mfernandasdias <91575360+mfernandadias@users.noreply.github.com>
2024-08-20 14:22:57 -0300

    exercícios 78 e 79 de vetores

--- ExerciciosLoiane/src/main/java/livro100Exercicios/vetores/Exer78.java
+++ ExerciciosLoiane/src/main/java/livro100Exercicios/vetores/Exer78.java
@@ -5,6 +5,31 @@ public class Exer78 {
     public static void main(String[] args){
         Scanner scan = new Scanner(System.in);
 
+        /* crie um programa que leia um vetor de números inteiros e
+        exiba quantas vezes um número específico aparece no vetor
+         */
+
+        System.out.println("Digite o tamanho do cetor que você deseja: ");
+        int tamanho = scan.nextInt();
+
+        int[] vetor = new int[tamanho];
+
+        System.out.println("Digite os elementos do vetor");
+        for(int i = 0; i < tamanho; i++){
+            vetor[i] = scan.nextInt();
+        }
+
+        System.out.println("Digite o número a ser buscado");
+        int numBuscando = scan.nextInt();
+
+         int contagem = 0;
+
+         for(int i = 0; i < tamanho; i++){
+             if(vetor[i] == numBuscando) {
+                 contagem++;
+             }
+         }
+         System.out.println("O número " + numBuscando + " aparece " + contagem + "vezes");
 
     }
 }
---
+++ ExerciciosLoiane/src/main/java/livro100Exercicios/vetores/Exer79.java
@@ -0,0 +1,12 @@
+package livro100Exercicios.vetores;
+import java.util.Scanner;
+public class Exer79 {
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+
+        /*
+        escreva um programa que leia dois vetores de números inteiros com o mesmo tamanho e exiba um novo
+        vetor com os elementos resultantes da multiplicação dos elementos correspondentes dos dois vetores 
+         */
+    }
+}
