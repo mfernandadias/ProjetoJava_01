c3549063d7784e1b2839c1fc1e61b4fc00a1edbf
mfernandasdias <91575360+mfernandadias@users.noreply.github.com>
2024-08-25 12:52:29 -0300

    adiciona vetor
    Java

---
+++ ExerciciosLoiane/src/main/java/estruturaDeDados/vetores/Aula03.java
@@ -0,0 +1,10 @@
+package estruturaDeDados.vetores;
+
+public class Aula03 {
+    public static void main(String[] args){
+        Vetor02 vetor = new Vetor02(5);
+
+        vetor.adiciona("elemento 1");
+        
+    }
+}
--- ExerciciosLoiane/src/main/java/estruturaDeDados/vetores/Vetor02.java
+++ ExerciciosLoiane/src/main/java/estruturaDeDados/vetores/Vetor02.java
@@ -6,4 +6,15 @@ public class Vetor02 {
     public Vetor02(int capacidade){
         this.elementos = new String[capacidade];
     }
+
+    public void adiciona(String elemento){
+        for(int i = 0; i<this.elementos.length; i++){
+            //
+            if(this.elementos[i] == null){
+                this.elementos[i] = elemento;
+                //parar o código na primeira posição que el eencontrar null
+                break;
+            }
+        }
+    }
 }
