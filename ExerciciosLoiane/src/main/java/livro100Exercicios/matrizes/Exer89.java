83d9cc41bcaa4d56d4849d815800e1a22bcbb2d1
mfernandasdias <91575360+mfernandadias@users.noreply.github.com>
2024-09-01 15:58:09 -0300

    estrtura de dados

--- ExerciciosLoiane/src/main/java/livro100Exercicios/matrizes/Exer89.java
+++ ExerciciosLoiane/src/main/java/livro100Exercicios/matrizes/Exer89.java
@@ -2,12 +2,49 @@ package livro100Exercicios.matrizes;
 import java.util.Scanner;
 
 public class Exer89 {
-    public static void main(String[]args){
+    public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         /*
         faça um programa que leia uma matriz 4x4 e verifique se ela é uma matriz diagonal,
         isto é, se todos os elementos fora da diagonal principal são iguais a zero
          */
+        int tamanho = 4;
+        int[][] matriz = new int[tamanho][tamanho];
+
+        //System.out.println("Digite os elementos da que vai preencher a matriz");
+        for (int i = 0; i < tamanho; i++) {
+            for (int j = 0; j < tamanho; j++) {
+                System.out.println("Digite o elemento da posição [" + i + "][" + j + "]");
+                matriz[i][j] = scan.nextInt();
+            }
+        }
+
+        boolean ehDiagonal = true;
+
+        //verificação da diagonal
+        for (int i = 0; i < tamanho; i++) {
+            for (int j = 0; j < tamanho; j++) {
+                if(i != j && matriz[i][j] != 0){
+                    ehDiagonal = false;
+                    break;
+                }
+            }
+            if(!ehDiagonal){
+                break;
+            }
+        }
+
+        //exibindo do resultado
+        if(ehDiagonal){
+            System.out.println("A matriz é diagonal");
+        } else {
+            System.out.println("A matriz não é diagonal");
+        }
+        scan.close();
+
 
     }
+
+
+
 }
--- ExerciciosLoiane/src/main/java/livro100Exercicios/matrizes/Exer90.java
+++ ExerciciosLoiane/src/main/java/livro100Exercicios/matrizes/Exer90.java
@@ -1,10 +1,75 @@
 package livro100Exercicios.matrizes;
+import java.util.Scanner;
 
 public class Exer90 {
     /*
     faça um programa que leia uma matriz m xn, indicando o local onde já minas de um
     jogo de campo minado (sendo
-    0 para campo neutro, e 1 para locais onde haveriam minas), e o programa deveria retornar uma matriz indicando, para
+    0 para campo neutro, e 1 para locais onde haveriam minas), e o programa deveria retornar
+    uma matriz indicando, para
     cada posição, o número de minas nas casas vizinhas
      */
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+
+        //leitura das dimensões da matriz
+        System.out.println("Digite o número de linhas da matriz");
+        int linhas = scan.nextInt();
+        System.out.println("Digite o número de colunas da matriz");
+        int colunas = scan.nextInt();
+
+        //criação da matriz
+        int[][] campoMinado = new int[linhas][colunas];
+
+        //leitura da matriz
+        System.out.println("Digite a matriz indicando os locais das minas");
+        System.out.println("0 para campo neutro e 1 para mina");
+
+        for(int i = 0; i < linhas; i++){
+            for(int j = 0; j < colunas; j++){
+                campoMinado[i][j] = scan.nextInt();
+            }
+        }
+
+        //criação damatriz com a contagem da minas nas casas vizinhas
+
+        //iteração pela matriz campoMinado
+        for(int i = 0; i < linhas; i++){
+            for(int j = 0; j < colunas; j++){
+                //verifique se a posição contém uma mina
+                if(campoMinado[i][j] == 1){
+                    //incrementado o número de minas nas casas vizinhas
+                    incrementarVizinhos(resultado, i, j);
+                }
+            }
+        }
+
+        //exibindo da matriz resultado
+        System.out.println("Matriz com a contagem de minas nas casas vizinhas: ");
+        for(int i = 0; i < colunas; i++ ){
+            for(int j = 0; j < colunas; j++){
+                System.out.print(resultado[i][j] + " ");
+            }
+            System.out.println();
+        }
+        scan.close();
+    }
+
+    //método para incrementar o número de minas nas casa vizinhas
+    public static void incrementarVizinhos(int[][] matriz,int linha,int coluna){
+        int linhas = matriz.length;
+        int colunas = matriz[0].length;
+
+        //verificando dos vizinhos em torno da posição atual
+        for(int i = linha -1; i <= linhas +1; i++){
+            for(int j = coluna - 1; j <= coluna + 1; j++){
+                //verifica sea posição é válida e não é a posição atual
+                if(i >= 0 && i < linhas && j >= 0 &&
+                j < colunas && !(i == linha && j == coluna)){
+                    matriz[i][j]++;
+                }
+            }
+        }
+    }
+
 }
